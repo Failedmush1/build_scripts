4ad11220d679d62025cd749bdeba8572e5fa4249
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-06-11 16:37:31 +0900

    Update Matrixx.sh

--- Matrixx.sh
+++ Matrixx.sh
@@ -4,7 +4,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/androi
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b matrixx device/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
 git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen  hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
