45a55a18f17cd06ad48c15c88262329ad52ca1f2
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-06-28 17:15:06 +0900

    Update Rising1.sh

--- Rising1.sh
+++ Rising1.sh
@@ -1,24 +1,16 @@
-#! /bin/bash
-
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/RisingOS-Revived-devices/device_xiaomi_apollo -b fifteen device/xiaomi/apollo && \
-git clone https://github.com/RisingOS-Revived-devices/vendor_xiaomi_apollo -b fifteen vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
-git clone https://github.com/MurtazaKolachi/keys.git -b rising vendor/lineage-priv/keys && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b rising1 device/xiaomi/renoir && \
+git clone https://gitlab.com/xiaomi_renoir/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
 # Vanilla Build
 . build/envsetup.sh && \
-riseup apollo user && make installclean && rise b; \
-rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
-cd out/target/product && mv apollo vanilla && cd ../../..; \
-# Gapps Build
-cd device/xiaomi/apollo && rm lineage_apollo.mk && mv gapps.txt lineage_apollo.mk && cd ../../..; \
-. build/envsetup.sh; \
-riseup apollo user && make installclean && rise b; \
-cd out/target/product && mv apollo gapps && cd ../../..; \
+riseup renoir user && make installclean && rise b; \
+rm -rf out/target/product/vanilla && \
+cd out/target/product && mv renoir vanilla && cd ../../..; \
