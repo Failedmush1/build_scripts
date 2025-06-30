9df27d3a9ba1e7bed68c1633a1c68dbe1c000c83
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-01 06:59:53 +0900

    Create Axion1.sh

---
+++ Axion1.sh
@@ -0,0 +1,12 @@
+rm -rf .repo/local_manifests && \
+rm -rf {device,vendor,kernel,hardware}/xiaomi && \
+rm -rf packages/apps/ViPER4AndroidFX && \
+repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 -git-lfs -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b axion device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush/vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \ -b main vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
+. build/envsetup.sh && \
+gk -s && axion renoir va user && make installclean && ax -b
