b6fee958d366fd478f3b33d8628ee5e0bb6b4f1c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-06-30 10:42:02 +0900

    Update Rising1.sh

--- Rising1.sh
+++ Rising1.sh
@@ -3,7 +3,7 @@ rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b rising1 device/xiaomi/renoir && \
-git clone https://gitlab.com/xiaomi_renoir/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
+git clone https://gitlab.com/renoir1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
 git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
