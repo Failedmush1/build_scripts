83e917fd33697672ddc47ae1aa1da0e73875c45a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-01 11:03:08 +0900

    Update Pixelage.sh

--- Pixelage.sh
+++ Pixelage.sh
@@ -1,1 +1,10 @@
+#! /bin/bash
 
+rm -rf .repo/local_manifests; \
+rm -rf {device,vendor,kernel,hardware}/xiaomi; \
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b fifteen --git-lfs -g default,-mips,-darwin,-notdefault && \
+/opt/crave/resync.sh && \
+git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b 15 device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
