1a8cf3189a8e189ffbd8e2f24745afad879b805d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-03 20:21:58 +0900

    Update Axion1.sh

--- Axion1.sh
+++ Axion1.sh
@@ -4,8 +4,8 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir && \
-git clone https://github.com/RobertGarciaa/device_xiaomi_sm8350-common -b 15 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_renoir -b Axion device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/android_device_xiaomi_sm8350-common -b 15 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/vendor_xiaomi_renoir -b 15 vendor/xiaomi/renoir && \
 git clone https://github.com/RobertGarciaa/vendor_xiaomi_sm8350-common -b 15 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
