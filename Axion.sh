be2e510dacd77ec9b039ad10ab3cdd5d07b839f4
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-03 15:50:08 +0900

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -8,4 +8,4 @@ git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main ve
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
 git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
 . build/envsetup.sh && \
-gk -s && axion renoir va user && make installclean && ax -b
+axion renoir va user && make installclean && ax -b
