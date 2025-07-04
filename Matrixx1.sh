a6c53a51e311a306212f1f7985c97c0b1c5402ea
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-04 15:34:49 +0900

    Update Matrixx1.sh

--- Matrixx1.sh
+++ Matrixx1.sh
@@ -2,6 +2,7 @@
 
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
+
 # ==== Fix for Trusty Soong Error (dummy module define) ====
 
 mkdir -p trusty/vendor/google/aosp/scripts/dummy
