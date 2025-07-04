506816e6a549ca308ae99a743b489e1f411dd8a8
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-04 14:57:41 +0900

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -1,5 +1,23 @@
 rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
+
+# ==== Fix for Trusty Soong Error (dummy module define) ====
+
+mkdir -p trusty/vendor/google/aosp/scripts/dummy
+
+cat > trusty/vendor/google/aosp/scripts/Android.bp << 'EOF'
+bootstrap_go_package {
+    name: "trusty_dirgroup_prebuilts_clang_host_linux-x86",
+    pkgPath: "trusty/vendor/google/aosp/scripts/dummy",
+    deps: [],
+    srcs: ["dummy.go"],
+}
+EOF
+
+echo "package dummy" > trusty/vendor/google/aosp/scripts/dummy/dummy.go
+
+# ==== End of Trusty Fix ====
+
 rm -rf packages/apps/ViPER4AndroidFX && \
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 -git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
