6fb1f991db0b4119ef73a086768d03c9afbaf2f2
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-04 11:31:10 +0900

    Update Matrix1.sh

--- Matrix1.sh
+++ Matrix1.sh
@@ -2,10 +2,12 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b matrixx device/xiaomi/renoir && \
-git clone https://gitlab.com/xiaomi_renoir/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
-git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen  hardware/xiaomi && \
+git clone https://github.com/Failedmush/device_xiaomi_renoir -b matrixx device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 15.2 device/xiaomi/sm8350-common && \
+git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://gitlab.com/senoved/android_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/kernel_xiaomi_sm8350 -b 15.2 kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush/hardware_xiaomi -b 15.2 hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
