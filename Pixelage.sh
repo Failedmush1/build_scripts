f24f7e4cfadd4757566dbf135a45ceec4dc418c4
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-04 14:58:40 +0900

    Update Pixelage.sh

--- Pixelage.sh
+++ Pixelage.sh
@@ -2,6 +2,24 @@
 
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
+
+# ==== Fix for Trusty Soong Error (dummy module define) ====
+
+mkdir -p trusty/vendor/google/aosp/scripts/dummy
+
+cat > trusty/vendor/google/aosp/scripts/Android.bp << 'EOF'
+bootstrap_go_package {
+    name: "trusty_dirgroup_prebuilts_clang_host_linux-x86",
+    pkgPath: "trusty/vendor/google/aosp/scripts/dummy",
+    deps: [],
+    srcs: ["dummy.go"],
+}
+EOF
+
+echo "package dummy" > trusty/vendor/google/aosp/scripts/dummy/dummy.go
+
+# ==== End of Trusty Fix ====
+
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectPixelage/android_manifest -b 15 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b 15 device/xiaomi/renoir && \
