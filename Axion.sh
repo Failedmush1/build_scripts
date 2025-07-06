a614e7a948b35c0ed32d7f835ff83a60545ee473
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-06 16:42:47 +0900

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -1,6 +1,18 @@
-rm -rf .repo/local_manifests && \
-rm -rf {device,vendor,kernel,hardware}/xiaomi && \
-rm -rf packages/apps/ViPER4AndroidFX && \
+! /bin/bash
+
+rm -rf .repo/local_manifests; \
+
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs; \
+
+rm -rf prebuilts/clang/host/linux-x86; \
+
+/opt/crave/resync.sh; \
+
+rm -rf out/target/product/renoir; \
+rm -rf device/xiaomi/renoir; \
+rm -rf kernel/xiaomi/sm8350; \
+rm -rf vendor/xiaomi/renoir; \
+rm -rf hardware/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 -git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
