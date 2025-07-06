4aa07d57b2f53f01b3e56946c34302a6726f9c39
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-06 15:10:48 +0900

    Update Rising.sh

--- Rising.sh
+++ Rising.sh
@@ -4,6 +4,7 @@ rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
 rm -rf packages/apps/ViPER4AndroidFX && \
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 -git-lfs -g default,-mips,-darwin,-notdefault && \
+rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b rising device/xiaomi/renoir && \
 git clone https://gitlab.com/xiaomi_renoir/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
