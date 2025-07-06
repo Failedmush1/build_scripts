d8d0145d305db48bbf708cfd397875cf8880b52a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-07 02:50:12 +0900

    Update Yapp.sh

--- Yapp.sh
+++ Yapp.sh
@@ -5,12 +5,12 @@ rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest -b sixteen --git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
-git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b yapp device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b Yapp device/xiaomi/renoir && \
 git clone https://github.com/RobertGarciaa/device_xiaomi_sm8350-common -b 15 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/vendor_xiaomi_renoir -b 15 vendor/xiaomi/renoir && \
 git clone https://github.com/RobertGarciaa/vendor_xiaomi_sm8350-common -b 15 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
-git clone https://github.com/RobertGarciaa/hardware_xiaomi -b 156277 hardware/xiaomi && \
+git clone https://github.com/RobertGarciaa/hardware_xiaomi -b 15 hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
