cb760f84a4faa9fd150a1234484da0c01cf9b29f
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-06 15:59:46 +0900

    Update Crdroid.sh

--- Crdroid.sh
+++ Crdroid.sh
@@ -1,7 +1,6 @@
 rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
-rm -rf packages/apps/ViPER4AndroidFX && \
-repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 -git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b crdroid device/xiaomi/renoir && \
