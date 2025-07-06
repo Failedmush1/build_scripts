606f6ed922a68d9b32ddff12ed220c73aad242b7
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-06 15:05:26 +0900

    Update Matrixx.sh

--- Matrixx.sh
+++ Matrixx.sh
@@ -1,6 +1,7 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectMatrixx/android -b 15.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
+rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b matrixx device/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
