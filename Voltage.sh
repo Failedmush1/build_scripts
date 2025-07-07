a9ed68f21dc8517519c46ca3afb3789899962a5a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-07 11:00:27 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -8,7 +8,13 @@ rm -rf device/xiaomi/renoir; \
 rm -rf kernel/xiaomi/renoir; \
 rm -rf vendor/xiaomi/vendor; \
 rm -rf hardware/xiaomi; \
-
+# Device Tree
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b voltage device/xiaomi/renoir && \
+# Vendor tree
+git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
+# Kernel
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
+# Hardware Xiaomi
 git clone https://github.com/VoltageOS-staging/hardware_xiaomi hardware/xiaomi; \
 . build/envsetup.sh; \
 brunch renoir
