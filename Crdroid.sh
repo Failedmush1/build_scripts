9993fbdf568c1dc9dbe2ea5ddee36595dd4b6552
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-07 14:51:40 +0900

    Update Crdroid.sh

--- Crdroid.sh
+++ Crdroid.sh
@@ -8,4 +8,4 @@ git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main ve
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
 git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
 . build/envsetup.sh && \
-renoir breakfast user && make installclean && mka bacon; \
+breakfast renoir user && make installclean && mka bacon; \
