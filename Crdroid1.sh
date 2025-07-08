b7e1a8c2497b8abd84a944c43b453dd5cb1111a3
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-08 18:06:15 +0900

    Update Crdroid1.sh

--- Crdroid1.sh
+++ Crdroid1.sh
@@ -5,7 +5,7 @@ rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir && \
 git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350 && \
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
