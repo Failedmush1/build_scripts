5bea12585021ffab44f0c05551e390ac563683c9
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-08 09:44:32 +0900

    Update Lineageos23.0.sh

--- Lineageos23.0.sh
+++ Lineageos23.0.sh
@@ -1,11 +1,11 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-23.0 -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid -b lineage-22.2 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b main device/xiaomi/renoir && \
-git clone https://gitlab.com/xiaomi_renoir/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
-git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
+git clone https://github.com/LineageOS/android_device_xiaomi_renoir -b lineage-22.2  device/xiaomi/renoir && \
+git clone https://gitlab.com/xiaomi_renoir/android_vendor_xiaomi_renoir -b lineage-22.2  vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2  kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-22.2  hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
