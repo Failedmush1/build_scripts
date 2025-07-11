0f74b4f289a1a1d3abc080e79a6ec0974808c07d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-11 12:02:18 +0900

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -10,12 +10,16 @@ rm -rf kernel/xiaomi/sm8350; \
 rm -rf vendor/xiaomi/renoir; \
 rm -rf hardware/xiaomi; \
 # Device Tree
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b axion device/xiaomi/renoir && \
-# Renoir Vendor
-git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
-# Kernel
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b axion1 device/xiaomi/renoir && \
+# Vendor tree
+git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
+# kernel
+git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
 # Hardware Xiaomi
-git clone https://github.com/AxionAOSP-devices/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
+git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
+export BUILD_USERNAME=Failedmush; \
+export BUILD_HOSTNAME=crave; \
+export TZ=Asia/Tokyo; \
 . build/envsetup.sh && \
 axion renoir va user && make installclean && ax -b
