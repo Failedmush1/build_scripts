44ff0ecd27139e88919cfdaa6b114e4ff3660d79
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-11 12:07:48 +0900

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -1,7 +1,7 @@
 ! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-23.0 -git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 -git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh; \
 rm -rf out/target/product/renoir; \
@@ -10,7 +10,7 @@ rm -rf kernel/xiaomi/sm8350; \
 rm -rf vendor/xiaomi/renoir; \
 rm -rf hardware/xiaomi; \
 # Device Tree
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b axion1 device/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b Axion1 device/xiaomi/renoir && \
 # Vendor tree
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
