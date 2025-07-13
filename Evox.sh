0ee7b0f3c57775633db28e79f5b01f31dd3eb196
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-13 14:43:00 +0100

    Update Evox.sh

--- Evox.sh
+++ Evox.sh
@@ -1,8 +1,21 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b vic -g default,-mips,-darwin,-notdefault && \
+
+#linux-x86 remove 
 rm -rf prebuilts/clang/host/linux-x86
-/opt/crave/resync.sh && \
+
+# repo init rom
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+# Build Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Sync Trees
 git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evolutionx device/xiaomi/renoir && \
 git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
@@ -12,13 +25,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 h
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
-# Vanilla Build
-. build/envsetup.sh && \
+# Set up build environment
+. build/envsetup.sh
+# Lunch
 lunch lineage_renoir-user && make installclean && m evolution; \
-rm -rf out/target/product/vanilla && rm -rf out/target/product/gapps; \
-cd out/target/product && mv renoir vanilla && cd ../../..; \
-# Gapps Build
-cd device/xiaomi/renoir && rm lineage_renoir.mk && mv gapps.txt lineage_renoir.mk && cd ../../..; \
-. build/envsetup.sh; \
-lunch lineage_renoir-user && make installclean && m evolution; \
-cd out/target/product && mv renoir gapps && cd ../../..; \
