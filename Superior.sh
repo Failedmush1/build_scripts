a7e097f26c5eedd87955b449ba5e4d1091e35518
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-14 13:40:29 +0900

    Create Superior.sh

---
+++ Superior.sh
@@ -0,0 +1,16 @@
+rm -rf .repo/local_manifests; \
+rm -rf {device,vendor,kernel,hardware}/xiaomi; \
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -g default,-mips,-darwin,-notdefault && \
+rm -rf prebuilts/clang/host/linux-x86
+/opt/crave/resync.sh && \
+git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b SuperiorOS device/xiaomi/renoir && \
+git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
+export BUILD_USERNAME=Failedmush; \
+export BUILD_HOSTNAME=crave; \
+export TZ=Asia/Tokyo; \
+. build/envsetup.sh && \
+breakfast renoir user && make installclean && m superior
