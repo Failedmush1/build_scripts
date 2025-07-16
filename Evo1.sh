915f4de028666a057062abd52fcd95fe5f68281a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-16 20:13:57 +0900

    Update Evo1.sh

--- Evo1.sh
+++ Evo1.sh
@@ -9,7 +9,7 @@ git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b line
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
 git clone https://github.com/Infinity-X-Devices/android_hardware_xiaomi -b 15 lineage-22.2 && \
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.3 hardware/dolby && \
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision hardware/dolby && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
