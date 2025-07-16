1600b7c8030561bf6eb5633c7e91819aea79d45c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-16 19:31:32 +0900

    Update and rename evo1.sh to Evo1.sh

--- Evo1.sh
+++ Evo1.sh
@@ -4,9 +4,9 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
 git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evolutionx device/xiaomi/renoir && \
-git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8350-common -b 15 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
-git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8350-common -b 15 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
 git clone https://github.com/Infinity-X-Devices/android_hardware_xiaomi -b 15 lineage-22.2 && \
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.3 hardware/dolby && \
