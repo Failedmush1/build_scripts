58e225372680c82da43707ab73364c97ae57c4dc
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-07-17 18:15:12 +0500

    Update Evo1.sh

--- Evo1.sh
+++ Evo1.sh
@@ -1,15 +1,19 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b vic -g default,-mips,-darwin,-notdefault && \
-rm -rf hardware/dolby 
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
+rm -rf device/xiaomi
 git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evolutionx device/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
+rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
+rm -rf kernel/xiaomi
 git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
+rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 && \
+rm -rf hardware/dolby 
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision hardware/dolby && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
