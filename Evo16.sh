013d78b3ab4e02560bc6c8b241d14a15b37b4039
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-23 02:12:44 +0900

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -3,7 +3,7 @@ rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b bka -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
-git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evolutionx device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evo16 device/xiaomi/renoir && \
 git clone https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/Spanish-or-Vanish/vendor_xiaomi_sm8350-common -b new vendor/xiaomi/sm8350-common && \
