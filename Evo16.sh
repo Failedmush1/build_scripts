a9cf8f928807581476c5dd232f8672fcc603af33
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-24 06:06:37 +0900

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -11,22 +11,22 @@ rm -rf device/xiaomi
 git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evo16 device/xiaomi/renoir && \
 
 # Device Common Tree
-git clone https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-23.0 device/xiaomi/sm8350-common && \
 
 # Vendor Tree
 rm -rf vendor/xiaomi
-git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 
 # Vendor Common Tree
-git clone https://github.com/Spanish-or-Vanish/vendor_xiaomi_sm8350-common -b new vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
 
 # Kernel Tree
 rm -rf kernel/xiaomi
-git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
 
 # Hardware Tree
 rm -rf hardware/xiaomi
-git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi && \
 
 # Dolby Tree
 rm -rf hardware/dolby 
