c5283ddbecf602b110791d38f060ee1d5551aadc
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-28 11:10:59 +0100

    Update Evo16.2.sh

--- Evo16.2.sh
+++ Evo16.2.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests; \
 
 # For OTA Updates
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b bka -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/Lunaris-AOSP/android -b 16 -g default,-mips,-darwin,-notdefault && \
 
 # Fix Trusty Vendor Errors
 rm -rf prebuilts/clang/host/linux-x86
@@ -11,17 +11,17 @@ rm -rf prebuilts/clang/host/linux-x86
 
 # Device Tree
 rm -rf device/xiaomi
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Evo16 device/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Luna device/xiaomi/renoir && \
 
 # Device Common Tree
-git clone https://github.com/kamikaonashi/android_device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b Back-up device/xiaomi/sm8350-common && \
 
 # Vendor Tree
 rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 
 # Vendor Common Tree
-git clone https://github.com/kamikaonashi/android_vendor_xiaomi_sm8350-common -b 16 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
 
 # Kernel Tree
 rm -rf kernel/xiaomi
