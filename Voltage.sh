aff16fd0d4a48281eab6a08ce2b33522c378fe5e
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-30 09:02:08 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -1,20 +1,32 @@
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/VoltageOS-staging/manifest -b 16 --git-lfs; \
-rm -rf prebuilts/clang/host/linux-x86; \
-/opt/crave/resync.sh; \
 
-rm -rf out/target/product/renoir; \
-rm -rf device/xiaomi/renoir; \
-rm -rf kernel/xiaomi/renoir; \
-rm -rf vendor/xiaomi/vendor; \
-rm -rf hardware/xiaomi; \
+# For OTA Updates
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 15-qpr2 -g default,-mips,-darwin,-notdefault && \
+
+# Fix Trusty Vendor Errors
+rm -rf prebuilts/clang/host/linux-x86
+
+# Sync Command
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
+
 # Device Tree
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b voltage device/xiaomi/renoir && \
-# Vendor tree
-git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
-# Kernel
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
-# Hardware Xiaomi
-git clone https://github.com/VoltageOS-staging/hardware_xiaomi hardware/xiaomi; \
-. build/envsetup.sh; \
-brunch renoir
+rm -rf device/xiaomi
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir && \
+
+# Device Common Tree
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b Back-up device/xiaomi/sm8350-common && \
+
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+
+# Vendor Common Tree
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/Failedmush/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350 && \
+
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
+start command
