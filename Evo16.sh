480c4980743927b43357cd12ceb8877bfd2125be
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-30 18:11:56 +0900

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -1,19 +1,7 @@
-#Fix Trusty Vendor Errors
-rm -rf prebuilts/clang/host/linux-x86
-# device Tree
-rm -rf device/xiaomi
 git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Evo16 device/xiaomi/renoir && \
-# Device Common Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b Back-up device/xiaomi/sm8350-common && \
-# Vendor Tree
-rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
-# Vendor Common Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
-# Kernel Tree
-rm -rf kernel/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
-# Hardware Tree
-rm -rf hardware/xiaomi
 git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
 start command
