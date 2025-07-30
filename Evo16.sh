e490625236d45639e275d914701830e7e1aaacc8
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-30 18:34:16 +0900

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b 
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
-git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi && \
+git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
