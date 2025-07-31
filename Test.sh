850ec5047a067703cf60240f3f8e3cbe9a804fba
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-01 08:14:33 +0900

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -4,18 +4,18 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 rm -rf device/xiaomi
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Infinity device/xiaomi/renoir && \
 # Device common Tree
-git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
+git clone https://github.com/Evolution-X-Devices/device_xiaomi_sm8350-common -b bka device/xiaomi/sm8350-common && \
 # Vendor Tree
 rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 # Vendor common Tree
-git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8350-common -b 16 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_sm8350-common -b bka vendor/xiaomi/sm8350-common && \
 # Kernel Tree
 rm -rf kernel/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
 # Hardware Tree
 rm -rf hardware/xiaomi
-git clone https://github.com/Failedmush1/android_hardware_xiaomiv2 -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/Evolution-X-Devices/hardware_xiaomi -b bka hardware/xiaomi
 # Hardware Dolby
 rm -rf hardware/dolby
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.3 hardware/dolby
