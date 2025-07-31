4f2c70445753cfd8230ac73cc4c305edaddf2261
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-07-31 23:48:33 +0900

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,22 @@
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+# Device Tree
+rm -rf device/xiaomi
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Infinity device/xiaomi/renoir && \
+# Device common Tree
+git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
+# Vendor common Tree
+git clone https://github.com/Infinity-X-Devices/vendor_xiaomi_sm8350-common -b 16 vendor/xiaomi/sm8350-common && \
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/Infinity-X-Devices/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+. build/envsetup.sh && \
+lunch infinity_renoir-user && \
+make installclean && \
+mka bacon
