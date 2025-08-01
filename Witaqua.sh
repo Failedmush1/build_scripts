32c0a06bcbe7b08238ef32c6d9598092ced6db53
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-02 05:42:32 +0900

    Update Witaqua.sh

--- Witaqua.sh
+++ Witaqua.sh
@@ -9,12 +9,12 @@ rm -rf device/xiaomi/renoir; \
 rm -rf kernel/xiaomi/sm8350; \
 rm -rf vendor/xiaomi/renoir; \
 rm -rf hardware/xiaomi; \
-git clone https://github.com/Failedmush/device_xiaomi_renoir -b 15.2 device/xiaomi/renoir && \
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 15.2 device/xiaomi/sm8350-common && \
+git clone https://github.com/WitAqua-Devices/device_xiaomi_renoir -b 15.2 device/xiaomi/renoir && \
+git clone https://github.com/WitAqua-Devices/device_xiaomi_sm8350-common -b 15.2 device/xiaomi/sm8350-common && \
 git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://gitlab.com/senoved/android_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
-git clone https://github.com/Failedmush/kernel_xiaomi_sm8350 -b 15.2 kernel/xiaomi/sm8350 && \
-git clone https://github.com/Failedmush/hardware_xiaomi -b 15.2 hardware/xiaomi && \
+git clone https://github.com/WitAqua-Devices/kernel_xiaomi_sm8350 -b 15.2 kernel/xiaomi/sm8350 && \
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 15.2 hardware/xiaomi && \
 export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
