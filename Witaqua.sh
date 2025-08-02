349037041eadefe05c15e463a21fb69b5b470baf
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-02 11:45:00 +0900

    Update Witaqua.sh

--- Witaqua.sh
+++ Witaqua.sh
@@ -13,7 +13,8 @@ git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.
 git clone git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 && \
 git clone https://github.com/Failedmush/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi 
-git clone https://github.com/Failedmush/vendor_xiaomi_camera-renoir -b fourteen vendor/xiaomixiaomiexport BUILD_USERNAME=Failedmush; \
+git clone https://github.com/Failedmush/vendor_xiaomi_camera-renoir -b fourteen vendor/xiaomixiaomi
+export BUILD_USERNAME=Failedmush; \
 export BUILD_HOSTNAME=crave; \
 export TZ=Asia/Tokyo; \
 . build/envsetup.sh && \
