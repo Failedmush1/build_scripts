a8cf8af4db722c12aaf6c7aeb63b325cb2b58b4e
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-04 08:28:11 +0900

    Update Witaqua.sh

--- Witaqua.sh
+++ Witaqua.sh
@@ -7,9 +7,9 @@ rm -rf device/xiaomi/renoir; \
 rm -rf kernel/xiaomi/sm8350; \
 rm -rf vendor/xiaomi/renoir; \
 rm -rf hardware/xiaomi; \
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir && \
 git clone https://github.com/WitAqua-Devices/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
-git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir
+git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://gitlab.com/senoved/android_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/WitAqua-Devices/kernel_xiaomi_sm8350 -b 16.0 kernel/xiaomi/sm8350 && \
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi 
