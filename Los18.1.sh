c6c4ed6182ce08b49df537a6e395715b3c9512c2
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-04 07:21:57 +0900

    Update Los20.sh

--- Los18.1.sh
+++ Los18.1.sh
@@ -4,11 +4,11 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS-Revived/android -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/renoir-development/android_device_xiaomi_renoir -b lineaeg-18.1 device/xiaomi/renoir && \
-git clone https://github.com/renoir-development/android_device_xiaomi_sm8350-common -b lineage-18.1 device/xiaomi/sm8350-common && \
-git clone https://gitlab.com/AndroPlus/proprietary_vendor_renoir -b lineage-18.1 vendor/xiaomi/renoir && \
-git clone https://github.com/renoir-development/android_kernel_xiaomi_sm8350 -b staging/lineage-18.1 kernel/xiaomi/sm8350 && \
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-18.1 hardware/xiaomi && \
+git clone https://github.com/felica-droid/android_device_xiaomi_renoir -b lineaeg-20 device/xiaomi/renoir && \
+git clone https://github.com/felica-droid/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common && \
+git clone https://gitlab.com/AndroPlus/proprietary_vendor_renoir -b lineage-20 vendor/xiaomi/renoir && \
+git clone https://github.com/felica-droid/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350 && \
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi && \
 . build/envsetup.sh && \
 # brunch configuration
 lunch lineage_renoir-userdebug && \
