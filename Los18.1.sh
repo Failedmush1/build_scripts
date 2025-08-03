f352a10c7d245cdc33c4eae9be4228a982b35207
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-04 07:45:32 +0900

    Update Los20.sh

--- Los18.1.sh
+++ Los18.1.sh
@@ -4,7 +4,7 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS-Revived/android -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/felica-droid/android_device_xiaomi_renoir -b lineaeg-20 device/xiaomi/renoir && \
+git clone https://github.com/felica-droid/android_device_xiaomi_renoir -b lineage-20 device/xiaomi/renoir && \
 git clone https://github.com/felica-droid/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
