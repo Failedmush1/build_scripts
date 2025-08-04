8e6a687875a7a7075c11d5e70eecd9d45b644bec
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-04 09:19:29 +0900

    Update Witaqua.sh

--- Witaqua.sh
+++ Witaqua.sh
@@ -13,9 +13,5 @@ git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.
 git clone https://gitlab.com/senoved/android_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/WitAqua-Devices/kernel_xiaomi_sm8350 -b 16.0 kernel/xiaomi/sm8350 && \
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi 
-git clone https://github.com/Failedmush/vendor_xiaomi_camera-renoir -b fourteen vendor/xiaomixiaomi
-export BUILD_USERNAME=Failedmush; \
-export BUILD_HOSTNAME=crave; \
-export TZ=Asia/Tokyo; \
 . build/envsetup.sh && \
 breakfast renoir user && make imakellclean && mka bacon
