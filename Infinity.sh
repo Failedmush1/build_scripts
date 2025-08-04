68fe933e43ef9ea42756e837eaadee8d3e5e945c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-04 18:06:08 +0900

    Update Infinity.sh

--- Infinity.sh
+++ Infinity.sh
@@ -4,7 +4,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # Device Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Luna device/xiaomi/renoir && \
 # Device common Tree
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
 # Vendor Tree
 git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 # Vendor common Tree
