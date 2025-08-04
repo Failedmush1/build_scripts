b2a64c308abc5761a7074e3d3573bd87235e4961
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-04 12:23:03 +0900

    Update Infinity.sh

--- Infinity.sh
+++ Infinity.sh
@@ -8,7 +8,7 @@ git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b 
 # Vendor Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 # Vendor common Tree
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
+git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 # Kernel Tree
 git clone https://github.com/Failedmush/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 && \
 # Hardware Tree
