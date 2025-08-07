5f17d37d6955f112de3015e7ce1f03415ba49f1d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-07 22:49:05 +0900

    Update Evo1.sh

--- Evo1.sh
+++ Evo1.sh
@@ -35,4 +35,4 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 h
 . build/envsetup.renoir-user && \
 
 # Lunch Commands
-lunchrenoir-usernoir-user && make installclean && m evolution; \
+lunch lineage renoir-user && make installclean && m evolution; \
