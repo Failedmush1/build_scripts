1f2740e431c57d68308c70a407c5a835a25a11a4
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-07 16:40:17 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -4,6 +4,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Voltageos device/xiaomi/renoir && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b Voltage device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/whyredfire/MiuiCamera-lisa -b master vendor/xiaomi/camera && \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
