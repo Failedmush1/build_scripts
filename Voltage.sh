bad0f186163d4e3dc4bbe7a1b71b3cc0de07d1b3
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-07 14:03:57 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -1,7 +1,7 @@
 repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
-git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Voltage device/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Voltageos device/xiaomi/renoir && \
 git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
