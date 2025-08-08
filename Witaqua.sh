f58808d4a6123cd1a8a489c0d1475c7aa0f63367
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-08 17:48:15 +0900

    Update Witaqua.sh

--- Witaqua.sh
+++ Witaqua.sh
@@ -2,16 +2,20 @@ rm -rf .repo/local_manifests; \
 repo init -u https://github.com/WitAqua/manifest.git -b 16.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86; \
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-rm -rf out/target/product/renoir; \
-rm -rf device/xiaomi/renoir; \
-rm -rf kernel/xiaomi/sm8350; \
-rm -rf vendor/xiaomi/renoir; \
-rm -rf hardware/xiaomi; \
+# Device Tree
+rm -rf device/xiaomi
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Witaqua device/xiaomi/renoir && \
-git clone https://github.com/WitAqua-Devices/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
-git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+# Device common Tree
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
+# Vendor common Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
-git clone https://github.com/Failedmush/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 && \
-git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi 
-. build/envsetup.sh && \
-breakfast renoir user && make makellclean && mka bacon
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
+. build/envsetup.sh
