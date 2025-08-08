1f84957929c210b135230e5cd194978198b5239c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-08 13:52:21 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs
+repo init -u https://github.com/VoltageOS-staging/manifest.git -b 16 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Voltageos device/xiaomi/renoir && \
