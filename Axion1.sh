4bb64605c7ecefd9874cd8ae2b89b70a072c6a11
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-08 14:07:20 +0900

    Update Axion1.sh

--- Axion1.sh
+++ Axion1.sh
@@ -3,13 +3,19 @@ rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
+# Device tree
 git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b Axion device/xiaomi/renoir && \
-git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
-git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
-git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
-export BUILD_USERNAME=Failedmush; \
-export BUILD_HOSTNAME=crave; \
-export TZ=Asia/Tokyo; \
-start command
+# Device common tree
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
+# Vendor Tree
+rm -rf vendor/xiaomi
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
+# Vendor common Tree
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
+# Kernel Tree
+rm -rf kernel/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
+# Hardware Tree
+rm -rf hardware/xiaomi
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
+. build/envsetup.sh
