1f5edfc4715d2f5e8fb6fb16507d8d3653e3a3ed
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-08 13:55:50 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -3,9 +3,8 @@ rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Voltageos device/xiaomi/renoir && \
 git clone https://github.com/Failedmush/device_xiaomi_sm8350-common/ -b Voltage device/xiaomi/sm8350-common && \
-git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
-git clone https://github.com/Failedmush/Miui-camera-renoir -b lineage-22.2 vendor/xiaomi/miuicamera-renoir && \
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
-git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0  kernel/xiaomi/sm8350 && \
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
 . build/envsetup.sh
