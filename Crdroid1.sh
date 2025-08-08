0df6db0adedf5123ddb5c829bee96a33d63236e6
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-08 17:09:12 +0900

    Update Crdroid1.sh

--- Crdroid1.sh
+++ Crdroid1.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -g default,-mips,-darwin,-notdefault && \
+repo init -u https://github.com/crdroidx/android.git -b 16.0 --git-lfs --no-clone-bundle
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 # Device Tree
