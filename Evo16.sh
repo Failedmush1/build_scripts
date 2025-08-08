46a95ee73a06ad9fbce44c3095ebf6691240b69a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-08 13:33:30 +0900

    Update Evo16.sh

--- Evo16.sh
+++ Evo16.sh
@@ -9,13 +9,11 @@ git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 devi
 # Vendor Tree
 rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
-# Miui Camera
-git clone https://github.com/Failedmush/Miui-camera-renoir -b lineage-22.2 vendor/xiaomi/miuicamera-renoir
 # Vendor common Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
 # Kernel Tree
 rm -rf kernel/xiaomi
-git clone https://github.com/Failedmush/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
 # Hardware Tree
 rm -rf hardware/xiaomi
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
