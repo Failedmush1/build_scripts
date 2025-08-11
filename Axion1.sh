43453f1d6e374afc5a8e208af1c09f1bda2879b6
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-11 18:39:31 +0900

    Update Axion1.sh

--- Axion1.sh
+++ Axion1.sh
@@ -1,6 +1,7 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-23.0 -g default,-mips,-darwin,-notdefault && \
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 # Device Tree
 rm -rf device/xiaomi
