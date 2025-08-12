cd031d68176fda27532ae0c4eedddeb24c7921b9
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-12 17:53:20 +0900

    Update Infinity.sh

--- Infinity.sh
+++ Infinity.sh
@@ -16,7 +16,7 @@ git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b linea
 # Hardware Tree
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
 rm -rf hardware/dolby
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.3 hardware/dolby
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 . build/envsetup.sh && 
 lunch infinity_renoir-user && \
 make installclean && \
