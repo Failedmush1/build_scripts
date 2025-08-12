ca7cb3c0113c71b3833079ea52143843fbc071a3
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-12 14:38:56 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -9,5 +9,6 @@ git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b line
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-22.2 kernel/xiaomi/sm8350 && \
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.3 hardware/dolby
 . build/envsetup.sh && \
 breakfast renoir user && make installclean && m superior
