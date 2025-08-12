8b540f6c97f5758628318e1f95a729ba2c15c8d6
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-12 14:32:41 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -3,14 +3,11 @@ rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b fifteen-los -m stable/latest.xml -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh && \
-git clone https://github.com/Failedmush/android_device_xiaomi_renoir -b SuperiorOS device/xiaomi/renoir && \
-git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b SuperiorOS device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
-git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-22.2 kernel/xiaomi/sm8350 && \
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
-export BUILD_USERNAME=Failedmush; \
-export BUILD_HOSTNAME=crave; \
-export TZ=Asia/Tokyo; \
 . build/envsetup.sh && \
 breakfast renoir user && make installclean && m superior
