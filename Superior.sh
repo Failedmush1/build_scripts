5ba70f98f8d58ddb6e82e0c33f6e30c93d37be09
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-12 14:38:10 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b fifteen-los -m stable/latest.xml -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
-/opt/crave/resync.sh && \
+repo sync --force-sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b SuperiorOS device/xiaomi/renoir && \
 git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
