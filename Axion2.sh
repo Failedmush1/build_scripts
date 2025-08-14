24c867ece0cc6a15a0ed3db5e553f4a877ae39a0
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-14 16:33:30 +0100

    Update Axion2.sh

--- Axion2.sh
+++ Axion2.sh
@@ -4,16 +4,14 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-22.2 --git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
-/opt/crave/resync.sh && \
-git clone https://github.com/Failedmush/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir && \
-git clone https://github.com/RobertGarciaa/device_xiaomi_sm8350-common -b 15 device/xiaomi/sm8350-common && \
-git clone https://github.com/Failedmush/vendor_xiaomi_renoir -b 15 vendor/xiaomi/renoir && \
-git clone https://github.com/RobertGarciaa/vendor_xiaomi_sm8350-common -b 15 vendor/xiaomi/sm8350-common && \
-git clone https://github.com/RobertGarciaa/android_kernel_xiaomi_sm8350 -b 15 kernel/xiaomi/sm8350 && \
-git clone https://github.com/RobertGarciaa/hardware_xiaomi -b 15 hardware/xiaomi && \
-export BUILD_USERNAME=Failedmush; \
-export BUILD_HOSTNAME=crave; \
-export PIXELAGE_BUILD="renoir"
-export TZ=Asia/Tokyo; \
+repo sync
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-22.2 kernel/xiaomi/sm8350 && \
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 source build/envsetup.sh && \
 axion renoir va user && make installclean && ax -b
