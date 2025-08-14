ffeb56537faff7a6f6c8a616a6e785167088107f
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-14 11:42:29 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -4,12 +4,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b SuperiorOS device/xiaomi/renoir && \
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir && \
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common && \
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-22.2 kernel/xiaomi/sm8350 && \
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi && \
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/m
 . build/envsetup.sh && \
 breakfast renoir user && make installclean && m superior
