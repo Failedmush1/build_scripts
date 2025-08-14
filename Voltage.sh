e6e60d4f38e729ffca4275b69b6948e5c9891490
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-14 10:22:45 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/VoltageOS-staging/manifest.git -b 16 --git-lfs
+repo init -u https://github.com/VoltageOS/manifest -b 16 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 # Device Tree
