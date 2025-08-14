005de7464071ff8da2b4dc9871ccf26bb5b6052c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-14 13:55:04 +0100

    Update Crdroid1.sh

--- Crdroid1.sh
+++ Crdroid1.sh
@@ -20,5 +20,5 @@ git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b linea
 rm -rf hardware/xiaomi
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 15.2 hardware/xiaomi
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision hardware/dolby
 . build/envsetup.sh
