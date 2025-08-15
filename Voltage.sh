67f1ab1fb3fbeb7d18ffbed0e8a378803de55e20
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-16 08:06:33 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -4,7 +4,7 @@ repo sync
 rm -rf device/xiaomi
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Voltageos device/xiaomi/renoir
 # Device common Tree
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b Voltage device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b Back-up-voltage device/xiaomi/sm8350-common
 # Vendor Tree
 rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir
