936f64a3ab9538e45f6b1d3dd65ddc4eea158d65
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-16 12:33:05 +0100

    Update Deep.sh

--- Deep.sh
+++ Deep.sh
@@ -4,7 +4,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 # Device Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Derp16 device/xiaomi/renoir && \
 # Device common Tree
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b lineage-23.0 device/xiaomi/sm8350-common && \
+git clonehttps://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
 # Vendor Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 # Vendor common Tree
