3503ba3c16f549435fc98a04acae526b04daf0ce
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-16 12:34:18 +0100

    Update Deep.sh

--- Deep.sh
+++ Deep.sh
@@ -4,13 +4,20 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 # Device Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Derp16 device/xiaomi/renoir && \
 # Device common Tree
-git clonehttps://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
 # Vendor Tree
+rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
+# Vendor camera
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 # Vendor common Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
 # Kernel Tree
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b lineage-23.0 kernel/xiaomi/sm8350 && \
+rm -rf kernel/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350 && \
 # Hardware Tree
+rm -rf hardware/xiaomi
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
+rm -rf hardware/dolby
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby && \
 . build/envsetup.sh
