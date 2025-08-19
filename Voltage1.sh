69a5ddaf9580862cd4633f0602ae8d891cc11d75
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-19 20:08:18 +0900

    Update Voltage1.sh

--- Voltage1.sh
+++ Voltage1.sh
@@ -9,13 +9,13 @@ git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main ve
 # Vendor camera
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 # Vendor common Tree
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b new vendor/xiaomi/sm8350-common 
 # Kernel Tree
 rm -rf kernel/xiaomi
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-22.2 kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350
 # Hardware Tree
 rm -rf hardware/xiaomi
-git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
 rm -rf hardware/dolby
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 . build/envsetup.sh
