6711e634fa11414afb6803699ca7cc30b5a4ba02
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-21 09:03:05 +0900

    Update Axion2.sh

--- Axion2.sh
+++ Axion2.sh
@@ -14,4 +14,4 @@ git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b linea
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi 
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 source build/envsetup.sh && \
-axion renoir va user && make installclean && ax -ax
+axion renoir va user && make installclean && ax -b
