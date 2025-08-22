2de5990b6ffd0047112e3b079f0177911610462f
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-22 12:44:16 +0100

    Update Axion2.sh

--- Axion2.sh
+++ Axion2.sh
@@ -2,7 +2,7 @@
 
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-23.0 --git-lfs -g default,-mips,-darwin,-notdefault && \
+repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-23.0 --git-lfs -g default,-mips,-darwin,-notdefault 
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir 
