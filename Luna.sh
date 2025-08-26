b1557a470198bd9509d748f1063a08e2e7f93edd
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-26 18:30:20 +0900

    Update Luna.sh

--- Luna.sh
+++ Luna.sh
@@ -12,7 +12,7 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lin
 # Miui camera
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 # Vendor common Tree
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.0 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common  && \
 # Kernel Tree
 rm -rf kernel/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350 && \
