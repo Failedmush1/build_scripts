c584001aadac2a9600c8e2fd73f802a432325471
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-01 20:11:43 +0900

    Update Crdroid1.sh

--- Crdroid1.sh
+++ Crdroid1.sh
@@ -7,12 +7,12 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 rm -rf device/xiaomi
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir && \
 # Device common Tree
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b Axionv2 device/xiaomi/sm8350-common && \
 # Vendor Tree
 rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 # Vendor common Tree
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_sm8350-common -b lineage-23 vendor/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Lunav2 vendor/xiaomi/sm8350-common && \
 # Kernel Tree
 rm -rf kernel/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350 && \
