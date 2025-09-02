b0c71ced062cadf692d6a4f42d71bcf9cc36c87c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-02 17:19:49 +0900

    Update Pixel experience legacy.sh

--- Pixel experience legacy.sh
+++ Pixel experience legacy.sh
@@ -5,7 +5,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 rm -rf device/xiaomi
 git clone https://github.com/PixelExperience-Devices/device_xiaomi_renoir -b thirteen device/xiaomi/renoir && \
 # Device common Tree
-git clone https://github.com/PixelExperience-Devices/device_xiaomi_sm7350-common -b thirteen device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-commonv3 -b thirteen device/xiaomi/sm8350-common && \
 # Vendor Tree
 rm -rf vendor/xiaomi
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir && \
