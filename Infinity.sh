b14bd0c29ab6a224d861a7ad0eecb0ff5b2e4213
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-11 11:37:32 +0900

    Update Infinity.sh

--- Infinity.sh
+++ Infinity.sh
@@ -4,7 +4,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # Device Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Infinity device/xiaomi/renoir && \
 # Device common Tree
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b Infinity device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b Infinityv2 device/xiaomi/sm8350-common && \
 # Vendor Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 # Vendor Miui camera
