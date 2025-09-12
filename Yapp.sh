c1b6bf29a4ebc9e130c048b500cb1989fcfb31f6
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-12 16:39:51 +0900

    Update Yapp.sh

--- Yapp.sh
+++ Yapp.sh
@@ -5,7 +5,7 @@ rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest -b sixteen --git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
 repo sync 
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Yapp device/xiaomi/renoir 
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Yaap device/xiaomi/renoir 
 git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b Axionv2 device/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
