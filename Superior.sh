62867e89eaf2fe8015fc3d29b9fe67f8cc599965
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-13 16:26:20 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -11,5 +11,6 @@ git clone https://gitlab.com/senoved/android_vendor_xiaomi_sm8350-common -b line
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-22.2 kernel/xiaomi/sm8350
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
+kernel/xiaomi/sm8350/kernelsetup.sh
 . build/envsetup.sh
-breakfast renoir user && make installclean && m superior
+breakfast renoir user && make installclean && mka bacon 
