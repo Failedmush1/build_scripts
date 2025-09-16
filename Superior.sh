489465430c92be79b41f77876c9fc17272cee63c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-16 16:37:59 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b fifteen-los -m stable/latest.xml -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
-repo sync --force-sync
+ --force-sync
 repo init --git-lfs
 rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
@@ -17,6 +17,7 @@ git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b linea
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 kernel/xiaomi/sm8350/kernelsetup.sh
+repo sync
 . build/envsetup.sh
 breakfast renoir user && make installclean && m superior 
 
