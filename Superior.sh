db7ede1044cb69a307f9a7ffc3c137c615cc5584
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-16 17:06:26 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b fifteen-los -m stable/latest.xml -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
-repo --force-sync
+repo sync --force-sync
 repo init --git-lfs
 rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
