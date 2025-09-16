5f0fc054ee6741300dc6cd52f9d79616d3e23d0a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-16 09:25:47 +0900

    Update Witaqua.sh

--- Witaqua.sh
+++ Witaqua.sh
@@ -2,8 +2,8 @@ rm -rf .repo/local_manifests; \
 repo init -u https://github.com/WitAqua/manifest.git -b 16.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Witaqua device/xiaomi/renoir && \
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b Axionv2 device/xiaomi/sm8350-common && \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Witaqua device/xiaomi/renoir
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b Axionv2 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Lunav2 vendor/xiaomi/sm8350-common 
