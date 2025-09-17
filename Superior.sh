094e1e1896560d578d3a81a0241ede426d08f446
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-17 09:25:14 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -7,6 +7,8 @@ repo init --git-lfs
 rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
 rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b fifteen-los -m stable/latest.xml -g default,-mips,-darwin,-notdefault && \
+rm -rf prebuilts/clang/host/linux-x86
 repo sync --force-sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b SuperiorOS device/xiaomi/renoir
 git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 15.2-with-Dolby-vision device/xiaomi/sm8350-common
@@ -17,7 +19,5 @@ git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b linea
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision-1.3 hardware/dolby
 kernel/xiaomi/sm8350/kernelsetup.sh
-repo sync
 . build/envsetup.sh
 breakfast renoir user && make installclean && m superior 
-
