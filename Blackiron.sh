7b2ad1aa88f9123c17d299e710898a0da00b41a6
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-17 21:47:22 +0900

    Update Blackiron.sh

--- Blackiron.sh
+++ Blackiron.sh
@@ -11,3 +11,5 @@ git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 kernel/xiaomi/sm8350/kernelsetup.sh
 .build/envsetup.sh
+blkilunch
+blki b
