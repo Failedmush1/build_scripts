afb75ff3457feca03f3dbad6aff0e81cdeb5e20e
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-09-24 23:06:42 +0900

    Update Deep.sh

--- Deep.sh
+++ Deep.sh
@@ -12,11 +12,10 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lin
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common
 # Kernel Tree
 rm -rf kernel/xiaomi
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b Lineage-23.0 kernel/xiaomi/sm8350
 # Hardware Tree
 rm -rf hardware/xiaomi
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
 rm -rf hardware/dolby
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
-kernel/xiaomi/sm8350/kernelsetup.sh
 . build/envsetup.sh
