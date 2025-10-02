a5986b7ef333d1d47267a4b4bd8520eb2212f7c6
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-02 15:20:34 +0900

    Update Lineage.sh

--- Lineage.sh
+++ Lineage.sh
@@ -1,13 +1,13 @@
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
+repo init -u https://github.com/Los-Ext/android_manifest -b 15.2 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common
-git clone https://github.com/Failedmush/Vendor-xiaomi-renoir -b lineage-23.0 vendor/xiaomi/renoir
+git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 15.2-with-Dolby-vision device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush/Vendor-xiaomi-renoir -b lineage-22.2 vendor/xiaomi/renoir
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv4 -b lineage-22.2 vendor/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b Lineage-23.0 kernel/xiaomi/sm8350
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b 15-back-up kernel/xiaomi/sm8350
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
 . build/envsetup.sh
 breakfast renoir user && make installclean && mka bacon
