c7c8523795ef1b274de60c3d2b58d89ca8c78693
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-04 17:39:51 +0900

    Update Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -15,7 +15,7 @@ git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 15.2-with
 git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv4 -b lineage-22.2 vendor/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v2 -b 15-back-up  kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v1 -b lineage-22.2  kernel/xiaomi/sm8350
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision-1.3 hardware/dolby
 rm -rf vendor/gapps
