47eb7e0dce70cbbe8c3ca71ecb91e4b36e2e5d57
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-10 10:47:05 +0900

    Update Crdroid1.sh

--- Crdroid1.sh
+++ Crdroid1.sh
@@ -20,5 +20,5 @@ git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware
 kernel/xiaomi/sm8350/kernelsetup.sh
 rm -rf hardware/lineage/compat
 git clone https://github.com/AxionAOSP/android_hardware_lineage_compat -b lineage-23.0 hardware/lineage/compat
- . build/envsetup.sh
- breakfast renoir user & mka bacon 
+. build/envsetup.sh
+breakfast renoir user & mka bacon 
