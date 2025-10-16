931d1b8e7f88226aa11d4800512188ce2f4b0276
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-16 15:30:01 +0900

    Update repo initialization command in Superior.sh

--- Superior.sh
+++ Superior.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b fifteen-los -m stable/latest.xml -g default,-mips,-darwin,-notdefault && \
+repo init -u https://github.com/SuperiorOS/manifest.git -b sixteen-los --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync --force-sync
 repo init --git-lfs
