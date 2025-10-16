80dc7443e232d34f7a6cdf1d062b0caa55a6f5ed
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-16 23:24:44 +0900

    Update Carlyxos.sh

--- Carlyxos.sh
+++ Carlyxos.sh
@@ -1,7 +1,7 @@
 repo init --git-lfs -u https://gitlab.com/CalyxOS/platform_manifest -b staging/android16
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b calyx device/xiaomi/renoir
 git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b Axionv2 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
