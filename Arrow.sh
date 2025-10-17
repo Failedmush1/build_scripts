0e7911ef79e6072cff094daa496f9af029c8fe44
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-17 12:50:41 +0900

    Update Arrow.sh

--- Arrow.sh
+++ Arrow.sh
@@ -1,9 +1,16 @@
-repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
+repo init -u https://github.com/ExtinctOS/manifest.git -b bka --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
-repo sync
-git clone https://github.com/LineageOS/android_device_xiaomi_renoir -b lineage-20 device/xiaomi/renoir
-git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-git clone https://github.com/LineageOS/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir 
+# Device common Tree
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b Axionv2 device/xiaomi/sm8350-common 
+# Vendor Tree
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
+# Vendor common Tree
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Lunav2 vendor/xiaomi/sm8350-common 
+# Kernel Tree
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
+# Hardware Tree
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
