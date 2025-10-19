eb59fb5abfd6a76be09238ef320cc137bdb57378
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-20 07:35:30 +0900

    Add kernel repository clone command for Xiaomi SM8350

--- Evo1.sh
+++ Evo1.sh
@@ -14,3 +14,4 @@ git clone https://gitlab.com/senoved/android_vendor_xiaomi_renoir -b lineage-21 
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-21 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-21 kernel/xiaomi/sm8350
