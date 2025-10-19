a6ef9152e1576ef01406ab163b48c831d2050188
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-19 09:01:04 +0900

    Update and rename Arrow.sh to CrdroidA13.sh

---
+++ CrdroidA13.sh
@@ -0,0 +1,11 @@
+repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
+rm -rf prebuilts/clang/host/linux-x86
+repo sync
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
+git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
