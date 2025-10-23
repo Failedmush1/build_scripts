2afcbe01f9e89c319f3a09c2ef2a0cb77eee9500
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-10-23 19:17:03 +0900

    Update Rising1.sh

--- Rising1.sh
+++ Rising1.sh
@@ -21,12 +21,6 @@ rm -rf hardware/xiaomi
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi && \
 rm -rf hardware/dolby
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby && \
-kernel/xiaomi/sm8350/kernelsetup.sh
-repo init --git-lfs
-rm -rf external/chromium-webview/prebuilt/*
-rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
-rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
-repo sync
 # =============================
 #  Build: Vanilla → Gapps
 # =============================
