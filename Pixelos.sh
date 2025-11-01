4995467a97e240d0def3a1098e5d4c7b07cb48f0
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-01 22:19:12 +0900

    Create Pixelos.sh

---
+++ Pixelos.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Pixelos device/xiaomi/renoir 
+# Device common Tree
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b Axionv2 device/xiaomi/sm8350-common 
+# Vendor Tree
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
+# Vendor common Tree
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Lunav2 vendor/xiaomi/sm8350-common 
+# Kernel Tree
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
+# Hardware Tree
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
