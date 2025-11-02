07c77890e4e20fbe2a83240ed6f0c2da74a78b70
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-02 21:04:16 +0900

    Update CrdroidA13.sh

--- CrdroidA13.sh
+++ CrdroidA13.sh
@@ -2,7 +2,7 @@ repo init -u https://github.com/Kyura-Ground/android -b 13.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
-git clone https://github.com/LineageOS/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
