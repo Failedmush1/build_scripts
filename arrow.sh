95aa442796b975d578d5e27083bb45bf22a61653
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-05 09:28:59 +0900

    Create arrow.sh

---
+++ arrow.sh
@@ -0,0 +1,12 @@
+repo init -u https://github.com/ArrowOS-T/android_manifest.git -b arrow-13.1
+repo sync
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Arrow device/xiaomi/renoir
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+. build/envsetup.sh
+lunch arrow_renoir-user
+m bacon
