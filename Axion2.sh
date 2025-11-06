8a8d240be2f8264fca94fc33effe73c2e5d39b89
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-06 11:51:58 +0900

    Update Axion2.sh

--- Axion2.sh
+++ Axion2.sh
@@ -10,10 +10,9 @@ git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b Axionv
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Lunav2 vendor/xiaomi/sm8350-common 
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi 
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
-kernel/xiaomi/sm8350/kernelsetup.sh
 . build/envsetup.sh  && \
 axion renoir va user && make installclean && ax -b
 axion renoir gms core && make installclean && ax -b
