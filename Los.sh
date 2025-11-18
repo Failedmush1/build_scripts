0453454efe204ed06291c3129b4f98d526f38013
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-18 09:45:18 +0900

    Update repository URLs and branches in Los.sh

--- Los.sh
+++ Los.sh
@@ -1,14 +1,15 @@
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
+repo init -u https://github.com/droid-legacy/android.git -b lineage-20.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b 15.2-with-Dolby-vision  device/xiaomi/sm8350-common
-git clone https://github.com/Failedmush/Vendor-xiaomi-renoir -b lineage-22.2 vendor/xiaomi/renoir
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
+git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv4 -b lineage-22.2 vendor/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision-1.3 hardware/dolby
-kernel/xiaomi/sm8350/kernelsetup.sh
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
 . build/envsetup.sh
-breakfast renoir user && make installclean && mka bacon
+lunch lineage_renoir-user
+m bacon
+git clone https://gitlab.com/EndCredits/android_vendor_xiaomi_sm8350-common -b thirteen vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv3 -b lineage-20 vendor/xiaomi/sm8350-common
