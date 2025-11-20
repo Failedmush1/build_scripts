581a3c0313080a204c76402898415db5a2e5d66a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-21 08:25:46 +0900

    Update Los.sh

--- Los.sh
+++ Los.sh
@@ -2,6 +2,7 @@ repo init -u https://github.com/droid-legacy/android.git -b lineage-20.0 --git-l
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
+rm -rf  device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
 git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
