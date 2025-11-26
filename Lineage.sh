d9405287c201c2834ea5800a450c73611bcf98a4
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-11-26 10:27:29 +0900

    Update Lineage to 23.1

--- Lineage.sh
+++ Lineage.sh
@@ -1,4 +1,4 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/Los-Ext/android_manifest.git -b 16 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android/ -b lineage-23.1 --git-lfs -g default,-mips,-darwin,-notdefault
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
