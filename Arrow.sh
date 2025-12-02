4ff199f9f92468bc40a50a2f6fb4cc55702bed5b
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-02 14:05:45 +0900

    Update Arrow.sh

--- Arrow.sh
+++ Arrow.sh
@@ -2,10 +2,10 @@ git init -u https://github.com/Arrow-OS-Extended/android_manifest.git -b arrow-1
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Arrow device/xiaomi/rrenoir
 rm -rf  device/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b Arrow device/xiaomi/sm8350-common
-rm-rf vendor/xiaomi/renoir
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b Arrow vendor/xiaomi/renoir
-git clone https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
+rm -rf  vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv3 -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
