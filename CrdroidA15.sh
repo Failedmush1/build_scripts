9300773c2ea6233ca6720d1a6f6ac001e61a62f9
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-05 19:55:07 +0900

    Update CrdroidA15.sh

--- CrdroidA15.sh
+++ CrdroidA15.sh
@@ -9,3 +9,7 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+. build/envsetup.sh
+brunch renoir user
+rm -rf device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-22.2 device/xiaomi/sm8350-common
