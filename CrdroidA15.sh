5ba8f08f28b5828e793ceefc2739def9920a625a
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-05 21:57:30 +0900

    Update CrdroidA15.sh

--- CrdroidA15.sh
+++ CrdroidA15.sh
@@ -5,6 +5,7 @@ git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b linea
 git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b 15.2-with-Dolby-vision device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+rm -rf  vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
