c228959f4b05f9f721e13f67c0caad850a04c4ce
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-05 15:18:04 +0900

    Update Crdroid.sh

--- Crdroid.sh
+++ Crdroid.sh
@@ -2,10 +2,11 @@ rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
-/opt/crave/resync.sh && \
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b crdroid device/xiaomi/renoir && \
-git clone https://github.com/Failedmush1/android_vendor_xiaomi_renoir -b main vendor/xiaomi/renoir && \
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b main kernel/xiaomi/sm8350 && \
-git clone https://github.com/Failedmush1/hardware_xiaomi -b fifteen hardware/xiaomi && \
-. build/envsetup.sh && \
-breakfast renoir user && make installclean && mka bacon; \
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
+git clone https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common -b A7 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush/Vendor-xiaomi-renoir -b lineage-22.2 vendor/xiaomi/renoir
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv4 -b lineage-22.2 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2 kernel/xiaomi/sm8350
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
