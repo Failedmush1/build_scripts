df2d423eacd02392f9af4536130f561830ed0f27
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-05 15:20:15 +0900

    Update Crdroid.sh

--- Crdroid.sh
+++ Crdroid.sh
@@ -1,7 +1,6 @@
-rm -rf .repo/local_manifests && \
-rm -rf {device,vendor,kernel,hardware}/xiaomi && \
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -git-lfs -g default,-mips,-darwin,-notdefault && \
 rm -rf prebuilts/clang/host/linux-x86
+repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-22.2 device/xiaomi/renoir
 git clone https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common -b A7 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir 
