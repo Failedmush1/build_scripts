01854816268101f40db49e645672469e5a8a407f
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-22 11:00:03 +0900

    Update Axion1.sh

--- Axion1.sh
+++ Axion1.sh
@@ -1,26 +1,14 @@
-rm -rf .repo/local_manifests; \
-rm -rf {device,vendor,kernel,hardware}/xiaomi; \
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android -b lineage-23.1 --git-lfs -g default,-mips,-darwin,-notdefault 
 rm -rf prebuilts/clang/host/linux-x86
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-# Device Tree
-rm -rf device/xiaomi
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir && \
-# Device common Tree
-git clone https://github.com/Failedmush/device_xiaomi_sm8350-common -b 16.0 device/xiaomi/sm8350-common && \
-# Vendor Tree
-rm -rf vendor/xiaomi
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir && \
-# Vendor camera
+repo sync
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir 
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.1 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
-# Vendor common Tree
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_sm8350-common -b lineage-23 vendor/xiaomi/sm8350-common && \
-# Kernel Tree
-rm -rf kernel/xiaomi
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350v3 -b lineage-23.0 kernel/xiaomi/sm8350 && \
-# Hardware Tree
-rm -rf hardware/xiaomi
-git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
-rm -rf hardware/dolby
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby && \
-. build/envsetup.sh
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
+git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi 
+git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
+. build/envsetup.sh  && \
+axion renoir va user && make installclean && ax -b
+axion renoir gms core user  && make installclea
