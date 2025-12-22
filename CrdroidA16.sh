0aab92f09f171dadc638df414abf762bb0e29efd
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-22 15:18:44 +0900

    Update CrdroidA16.sh

--- CrdroidA16.sh
+++ CrdroidA16.sh
@@ -4,7 +4,7 @@ repo sync
 # Device Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Crdroid device/xiaomi/renoir 
 # Device common Tree
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b lineage-23.1 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.1 device/xiaomi/sm8350-common 
 # Vendor Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
 # Vendor common Tree
