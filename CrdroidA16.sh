4886c4b827a1c2e8f3b2fd232a30ee32acaeeb1c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-22 15:07:59 +0900

    Update CrdroidA16.sh

--- CrdroidA16.sh
+++ CrdroidA16.sh
@@ -4,19 +4,17 @@ repo sync
 # Device Tree
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Crdroid device/xiaomi/renoir 
 # Device common Tree
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b Axionv2 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b lineage-23.1 device/xiaomi/sm8350-common 
 # Vendor Tree
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
 # Vendor common Tree
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Lunav2 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
 # Kernel Tree
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
 # Hardware Tree
 git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
-rm -rf hardware/lineage/compat
-git clone https://github.com/AxionAOSP/android_hardware_lineage_compat -b lineage-23.0 hardware/lineage/compat
 . build/envsetup.sh
 brunch renoir user
 export GMS=true
