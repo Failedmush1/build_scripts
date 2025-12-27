4a2f001b24390103ee251cf6f25cf5c10b6f3c9d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-27 14:17:13 +0900

    Update Axion16.1.sh

--- Axion16.1.sh
+++ Axion16.1.sh
@@ -17,4 +17,4 @@ git clone https://github.com/inferno0230/hardware_dolby -b sixteen-qpr1 hardware
 axion renoir va user && make installclean && ax -b
 axion renoir gms core user  && make installclean
 rm -rf hardware/dolby
-git clone https://github.com/Tashar02/vendor_oneplus_dolby -b main hardware/dolby
+git clone https://github.com/Tashar02/vendor_oneplus_dolby -b main vendor/oneplus/dolby
