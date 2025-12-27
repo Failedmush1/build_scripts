96e478f28487c0a396902dd6e20c2adec31a4a8d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-27 14:15:48 +0900

    Update Axion16.1.sh

--- Axion16.1.sh
+++ Axion16.1.sh
@@ -8,6 +8,7 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lin
 git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
+git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 rm -rf hardware/dolby
