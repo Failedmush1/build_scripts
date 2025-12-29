0c7be5260bcd64ed11a87cd5af52c1cc218cc3ec
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-12-29 18:11:55 +0900

    Update Pbrp.sh

--- Pbrp.sh
+++ Pbrp.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# Create Directory for OrangeFox
+mkdir OrangeFox
+cd OrangeFox
+
+# Just to be on safer side
+export OUT_DIR=out
+ulimit -n 16000
+
+# Clone and Sync OrangeFox
+git clone https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-12.1
+cd sync
+.git -b android-12.1 && \
+repo sync
+
+# Clone OrangeFox tree
+cd ~/OrangeFox/android-12.1
+rm -rf device/xiaomi/renoir
+git clone https://github.com/Failedmush1/twrp_device_xiaomi_renoir -b fox-12.1-miui ./device/xiaomi/renoir
+
+# Build Environment
+set +e
+source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
+set -e
+lunch twrp_renoir-eng && make clean && mka adbd bootimage
