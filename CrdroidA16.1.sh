15390020afcb26707045310973b585d380e41dcf
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-19 19:22:16 +0900

    Fix build command syntax in CrdroidA16.1.sh

--- CrdroidA16.1.sh
+++ CrdroidA16.1.sh
@@ -9,7 +9,6 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
-. build/envsetup.sh  && \
 . build/envsetup.sh
 brunch renoir user
 export GMS=true
