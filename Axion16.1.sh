1265641016aaed8de36e6e5cee353a12ca630707
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-19 19:20:07 +0900

    Update kernel branch for Xiaomi SM8350

--- Axion16.1.sh
+++ Axion16.1.sh
@@ -9,19 +9,10 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lin
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 rm -rf vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1 kernel/xiaomi/sm8350 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 rm -rf hardware/dolby
 git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
 . build/envsetup.sh  && \
 axion renoir va user && make installclean && ax -b
 axion renoir gms core user  && make installclean && ax -b
-rm -rf hardware/dolby
-git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
-rm -rf device/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b lineage-23.1 device/xiaomi/sm8350-common
-rm -rf vendor/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv2 -b lineage-23.1 vendor/xiaomi/sm8350-common 
-rm -rf kernel/xiaomi/sm8350
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b Sukisu  kernel/xiaomi/sm8350 
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350 
