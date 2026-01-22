366d0654ffd4adaf5f6ab719f715e5fc7a50b943
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-22 11:58:15 +0900

    Update Mica.sh

--- Mica.sh
+++ Mica.sh
@@ -1,21 +1,17 @@
-repo init -u https://github.com/Project-Mica/manifest -b 16-qpr1
+repo init -u https://github.com/Project-Mica/manifest -b 16-qpr2
 rm -rf prebuilts/clang/host/linux-x86
 repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Mica device/xiaomi/renoir 
 # Device common Tree
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-commonv2 -b Mica device/xiaomi/sm8350-common 
-# Vendor Tree
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.0 vendor/xiaomi/renoir 
-# Vendor common Tree
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.1 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.1 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+rm -rf vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
-# Kernel Tree
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.0 kernel/xiaomi/sm8350 
-# Hardware Tree
-git clone https://github.com/WitAqua-Devices/hardware_xiaomi -b 16.0 hardware/xiaomi
-git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir vendor/xiaomi/camera
-rm -rf hardware/lineage/compat
-git clone https://github.com/AxionAOSP/android_hardware_lineage_compat -b lineage-23.0 hardware/lineage/compat
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b sony-1.2 hardware/dolby
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350  
+git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
+rm -rf hardware/dolby
+git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
 cd vendor/gms
 bash generate-gms.sh
 . build/envsetup.sh
