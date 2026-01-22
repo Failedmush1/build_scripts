559fe9c396498845005d26fa90f83ab13b209ee4
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-22 11:19:34 +0900

    Update Axion16.1.sh

--- Axion16.1.sh
+++ Axion16.1.sh
@@ -10,7 +10,7 @@ git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 ven
 rm -rf vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350  
-git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
+git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 rm -rf hardware/dolby
 git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
 . build/envsetup.sh  && \
