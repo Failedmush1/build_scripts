72503be475752174b7f6378f0d950fac286c69d2
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-25 19:51:41 +0900

    Update kernel branch for Xiaomi SM8350

--- CrdroidA16.1.sh
+++ CrdroidA16.1.sh
@@ -6,7 +6,7 @@ git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.1 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350  
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
 . build/envsetup.sh
