551d5043135715ecebe7b799307fbd9bd988d78e
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-26 20:28:13 +0900

    Update Axion16.2.sh

--- Axion16.2.sh
+++ Axion16.2.sh
@@ -5,10 +5,10 @@ rm -rf device/xiaomi/renoir
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Axion device/xiaomi/renoir 
 rm -rf device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.1 device/xiaomi/sm8350-common 
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.1 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.2 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 rm -rf vendor/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.1 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1-test  kernel/xiaomi/sm8350  
 git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
 rm -rf hardware/dolby
