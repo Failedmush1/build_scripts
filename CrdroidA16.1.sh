55594a9e5dba1190f80e97cf555fb1d46317cf8d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-26 23:28:09 +0900

    Add vendor_bcr repository clone command

--- CrdroidA16.1.sh
+++ CrdroidA16.1.sh
@@ -9,6 +9,7 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 . build/envsetup.sh
 brunch renoir user
 export GMS=true
