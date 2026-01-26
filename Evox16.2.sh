06d2f127f747d132a21217e31a95e71c7178e566
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-26 20:29:25 +0900

    Add Evox16.2.sh script for Evolution X setup

---
+++ Evox16.2.sh
@@ -0,0 +1,16 @@
+repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Evox device/xiaomi/renoir 
+rm -rf device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.2 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.2 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+rm -rf vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.2  kernel/xiaomi/sm8350  
+git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
+rm -rf hardware/dolby
+git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+. build/envsetup.sh  && \
+lunch lineage_renoir-user
+m evolution
