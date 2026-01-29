716a61705f599f97f0acfe2d1be819b70e678bdb
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-01-29 18:11:36 +0900

    Create Yaap.sh

---
+++ Yaap.sh
@@ -0,0 +1,14 @@
+repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs
+repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Yaap device/xiaomi/renoir
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.2 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.2 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.2  kernel/xiaomi/sm8350  
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
+git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
+source build/envsetup.sh
+lunch yaap_renoir-user && m yaap
+
