c14c24e2a0679a33bf824888daca8bbee58b9c18
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-03-12 12:14:27 +0900

    Update twrp.sh

--- twrp.sh
+++ twrp.sh
@@ -20,6 +20,7 @@ git clone https://github.com/Failedmush1/twrp_device_xiaomi_renoir -b Test2 devi
 set +e
 source build/envsetup.sh
 export TW_INCLUDE_RECOVERY_INSTALLER=true
+export TARGET_RECOVERY_DEVICE_MODULES="recovery_installer
 export ALLOW_MISSING_DEPENDENCIES=true
 set -e
 lunch twrp_renoir-eng && make clean && mka adbd bootimage
