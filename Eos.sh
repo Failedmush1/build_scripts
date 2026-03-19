8b938e96c8f7f40a9c580642edbe4fc775e489fc
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-03-19 17:51:22 +0000

    Add files via upload

---
+++ Eos.sh
@@ -0,0 +1,16 @@
+repo init -u https://gitlab.e.foundation/e/os/android.git -b a16 --git-lfs
+repo sync
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Crdroid device/xiaomi/renoir 
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.2 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.2 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.2  kernel/xiaomi/sm8350  
+git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
+git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
+git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b lineageos vendor/lineage-priv/keys
+cd vendor/lineage-priv/keys
+./keys.sh
+. build/envsetup.sh
+brunch renoir user
