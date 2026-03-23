8cac16bcf3eef77ee16b9034a009b474a0640ca1
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-03-23 15:06:04 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -10,7 +10,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 h
 git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
 git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 rm -rf vendor/voltage-priv/keys
-git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b 16.2 vendor/voltage-priv/keys
+git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b Voltageos vendor/voltage-priv/keys
 cd vendor/voltage-priv/keys
 ./keys.sh
 . build/envsetup.sh
