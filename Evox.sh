11c5e26280c29935e29d8fce4a5c26d8076a6a0d
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-03-28 20:49:36 +0900

    Update Evox.sh

--- Evox.sh
+++ Evox.sh
@@ -10,7 +10,7 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.1  kernel/xiaomi/sm8350  
 git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.1 hardware/xiaomi 
 rm -rf hardware/dolby
-git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.3 hardware/dolby
 git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b lineageos vendor/lineage-priv/keys
 cd vendor/lineage-priv/keys
 ./keys.sh
