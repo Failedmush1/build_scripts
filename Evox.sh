b16709e082d6fbd39fcbcf028d4e33c8dbf3b9d9
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-03-29 20:04:20 +0900

    Update Evox.sh

--- Evox.sh
+++ Evox.sh
@@ -14,3 +14,19 @@ chmod +x create-signed-env.sh
 ./create-signed-env.sh
 . build/envsetup.sh
 brunch renoir user
+rm -rf repo init --depth=1 -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Evox device/xiaomi/renoir 
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2-test  kernel/xiaomi/sm8350  
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi 
+git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep -b lineage-22.2 hardware/sony/timekeep
+wget https://raw.githubusercontent.com/306bobby-android/crDroid-build-signed-script/main/create-signed-env.sh
+chmod +x create-signed-env.sh
+./create-signed-env.sh
+rm -rf hardware/dolby
+git clone https://github.com/Failedmush1/hardware_dolby -b Renoir-Dolby hardware/dolb
