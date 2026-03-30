a84634e1f5c5214d5ba00f037e6012521f9fd86b
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-03-31 06:47:44 +0900

    Create lineageA13.sh

---
+++ lineageA13.sh
@@ -0,0 +1,15 @@
+repo init -u https://github.com/LineageOS-T/android.git -b lineage-20.0 --git-lfs --depth=1
+rm -rf prebuilts/clang/host/linux-x86
+repo sync -c --force-sync
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-20 device/xiaomi/renoir
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2-test kernel/xiaomi/sm8350
+wget https://raw.githubusercontent.com/306bobby-android/crDroid-build-signed-script/main/create-signed-env.sh
+chmod +x create-signed-env.sh
+./create-signed-env.sh
+. build/envsetup.sh
+brunch renoir user
