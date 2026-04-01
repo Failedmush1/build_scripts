066ce03b3cd1304c6107a8aa323fcf4e145a30b3
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-01 17:18:28 +0900

    Update LosA13.sh

--- LosA13.sh
+++ LosA13.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b linea
 git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
-git clone https://github.com/Failedmush/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2-test kernel/xiaomi/sm8350
 wget https://raw.githubusercontent.com/306bobby-android/crDroid-build-signed-script/main/create-signed-env.sh
