ae6ca0de76da00358e4893e008daec14d16aec64
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-01 20:03:36 +0900

    Update LosA13.sh

--- LosA13.sh
+++ LosA13.sh
@@ -2,9 +2,9 @@ repo init -u https://github.com/LineageOS/android -b lineage-20.0 --git-lfs --de
 rm -rf prebuilts/clang/host/linux-x86
 repo sync 
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-20 device/xiaomi/renoir
-git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv2 -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2-test kernel/xiaomi/sm8350
