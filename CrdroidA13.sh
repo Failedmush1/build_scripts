2f6e2bbce9a4c6e11457d292d1a48a1853e43653
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-03 11:39:06 +0900

    Update CrdroidA13.sh

--- CrdroidA13.sh
+++ CrdroidA13.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lin
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv2 -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-22.2-test kernel/xiaomi/sm8350
 . build/envsetup.sh
 lunch lineage_renoir-user
 m bacon
