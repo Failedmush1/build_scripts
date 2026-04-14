9ad3b5507acf7e76a01692f265fbea8672c8ee07
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-14 15:29:43 +0900

    Update CrdroidA16.2.sh

--- CrdroidA16.2.sh
+++ CrdroidA16.2.sh
@@ -8,7 +8,7 @@ git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 ven
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.2  kernel/xiaomi/sm8350  
 git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
-git clone https://github.com/Failedmush1/hardware_dolby -b Dolby-Vision-1.2 hardware/dolby
+git clone https://github.com/Failedmush1/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
 git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b lineageos vendor/lineage-priv/keys
 cd vendor/lineage-priv/keys
