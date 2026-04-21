6da0b990e7566291193e5d018a2dee0ef877edaa
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-22 05:39:49 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 ven
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b lineage-23.2v2  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
-git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
+git clone https://github.com/Failedmush1/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
 git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 rm -rf vendor/voltage-priv/keys
 git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b Voltageos vendor/voltage-priv/keys
@@ -15,7 +15,3 @@ cd vendor/voltage-priv/keys
 ./keys.sh
 . build/envsetup.sh
 brunch renoir user
-rm -rf  device/xiaomi/sm8350-common
-git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b test device/xiaomi/sm8350-common
-rm -rf kernel/xiaomi/sm8350
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b Test2  kernel/xiaomi/sm8350  
