d6f14741236408fc8603b8f12874516c2013c08c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-22 05:16:56 +0900

    Update Caryxos.sh

--- Caryxos.sh
+++ Caryxos.sh
@@ -8,5 +8,6 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT  kernel/xiaomi/sm8350  
 git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
 git clone https://github.com/Failedmush1/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
+git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 . build/envsetup.sh
  breakfast renoir user & m otapackage
