4995fcea89c477a05c828ebd9af2918a35416119
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-21 20:50:28 +0900

    Update Caryxos.sh

--- Caryxos.sh
+++ Caryxos.sh
@@ -8,3 +8,5 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT  kernel/xiaomi/sm8350  
 git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
 git clone https://github.com/Failedmush1/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
+. build/envsetup.sh
+brunch renoir user
