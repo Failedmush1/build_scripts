ab717a8046853df98181e11569858e000684cb95
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-28 16:17:05 +0900

    Update Eos.sh

--- Eos.sh
+++ Eos.sh
@@ -1,6 +1,6 @@
 repo init -u https://gitlab.e.foundation/e/os/android.git -b a15 --git-lfs
 repo sync
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b Crdroid device/xiaomi/renoir 
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b Crdroid device/xiaomi/renoir 
 git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-22.2 device/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-22.2 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
