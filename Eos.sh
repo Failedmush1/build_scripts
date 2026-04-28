c702896de16d32c34d8a9de06691ccd1f36128ef
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-04-28 16:27:57 +0900

    Update Eos.sh

--- Eos.sh
+++ Eos.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 ven
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-22.2 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT  kernel/xiaomi/sm8350  
 git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-22.2 hardware/xiaomi 
-git clone https://github.com/Failedmush/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
+git clone https://github.com/Failedmush1/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
 git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
 git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b lineageos vendor/lineage-priv/keys
 cd vendor/lineage-priv/keys
