635a60d2d64e4efed2b2e0fe4acb50f74edf135e
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-05-06 15:14:48 +0900

    Update CrdroidA13.sh

--- CrdroidA13.sh
+++ CrdroidA13.sh
@@ -1,15 +1,16 @@
-repo init -u https://github.com/crDroid-Resurrected/android.git -b 13.0 --git-lfs
+repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86
 repo sync
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b lineage-20 device/xiaomi/renoir
-git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-commonv2 -b lineage-20 vendor/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
-git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b Renoir-new-base kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/vendor_bcr -b A13 vendor/bcr
+wget https://raw.githubusercontent.com/306bobby-android/crDroid-build-signed-script/main/create-signed-env.sh
+chmod +x create-signed-env.sh
+./create-signed-env.sh
 . build/envsetup.sh
-lunch lineage_renoir-user
-m bacon
-rm -rf device/xiaomi/renoir
-git clone https://github.com/Failedmush1/android_device_xiaomi_renoirv2 -b lineage-20 device/xiaomi/renoir
+brunch renoir user
