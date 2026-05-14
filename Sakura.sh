863bdafd9c7543f7983f555130ef9451c4243998
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-05-14 15:13:03 +0900

    Update Sakura.sh

--- Sakura.sh
+++ Sakura.sh
@@ -10,3 +10,4 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 h
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
 source build/envsetup.sh
 lunch lineage_renoir-bp4a-user & m bacon
+ git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.2v2 device/xiaomi/sm8350-common 
