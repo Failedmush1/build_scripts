028c233bf4bab92fd0a9ea13ddb6dbee57bac6c0
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-05-23 08:10:44 +0900

    Update RisingosA13.sh

--- RisingosA13.sh
+++ RisingosA13.sh
@@ -1,7 +1,7 @@
 repo init -u https://github.com/RisingOS-LTS/manifest -b thirteen --git-lfs
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b RisingA13 device/xiaomi/renoir
-git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-commonv2 -b lineage-20 device/xiaomi/sm8350-common
+git clone https://github.com/Failedmush1/android_device_xiaomi_sm8350-common -b lineage-20 device/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-20 vendor/xiaomi/renoir
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
