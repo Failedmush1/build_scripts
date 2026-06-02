1f9c38c9c8f7614ddb3184b15c30ae6a77411f7c
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-06-03 07:41:11 +0900

    Update RisingosA13.sh

--- RisingosA13.sh
+++ RisingosA13.sh
@@ -7,6 +7,9 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT kernel/xiaomi/sm8350
+git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b A13-aosp vendor/extra/keys
+cd vendor/extra/keys
+./keys.sh
 . build/envsetup.sh
 lunch lineage_renoir-user
 mka bacon
