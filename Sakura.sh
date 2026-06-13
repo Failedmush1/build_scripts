61cc9ff7e247109f84df533acac9008806a1f177
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-06-13 22:36:07 +0900

    Update Sakura.sh

--- Sakura.sh
+++ Sakura.sh
@@ -2,7 +2,7 @@
  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
  git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b sakura device/xiaomi/renoir
  git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b lineage-23.2 device/xiaomi/sm8350-common 
-git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.2v2 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b lineage-23.2 vendor/xiaomi/renoir 
 git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT  kernel/xiaomi/sm8350  
