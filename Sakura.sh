9f6e7eab69a2e9dad65f701d57a27752930f1fde
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-06-13 21:54:12 +0900

    Update Sakura.sh

--- Sakura.sh
+++ Sakura.sh
@@ -8,6 +8,10 @@ git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
 git clone https://github.com/failedmush1/hardware_dolby -b Dolby-Vision-2.6 hardware/dolby
+git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
+git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b lineageos vendor/lineage-priv/keys
+cd vendor/lineage-priv/keys
+./keys.sh
 source build/envsetup.sh
 brunch renoir user
 lunch lineage_renoir-bp4a-user & m bacon
