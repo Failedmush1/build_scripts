5dcc9c710dda09cef7fe325bd6184de55dec039b
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-07-05 17:42:59 +0900

    Update Voltage.sh

--- Voltage.sh
+++ Voltage.sh
@@ -7,8 +7,7 @@ git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 ven
 git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b lineage-23.2 vendor/xiaomi/sm8350-common 
 git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b KSUNEXT  kernel/xiaomi/sm8350  
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
-git clone https://github.com/failedmush1/hardware_dolby -b Dolby-Vision-2.1 hardware/dolby
-git clone https://github.com/Failedmush1/vendor_bcr -b main vendor/bcr
+git clone https://github.com/failedmush1/hardware_dolby -b Dolby-Vision-2.6 hardware/dolby
 rm -rm -rf vendor/voltage-priv/keys
 git clone https://github.com/Failedmush1/vendor_voltage-priv_keys -b Voltageos vendor/voltage-priv/keys
 cd vendor/voltage-priv/keys
