1d4feafbd3dbaa132a3f67265ecdffb30670d3df
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-07-09 09:42:01 +0900

    Create CLO.sh

---
+++ CLO.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b Neoteric-A16 device/xiaomi/renoir 
+git clone https://github.com/Failedmush1/device_xiaomi_sm8350-common -b Neoteric-A16 device/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_renoir -b Neoteric-A16 vendor/xiaomi/renoir 
+git clone https://github.com/Failedmush1/vendor_xiaomi_camera -b Renoir-16.1 vendor/xiaomi/camera
+git clone https://github.com/Failedmush1/proprietary_vendor_xiaomi_sm8350-common -b Neoteric-A16 vendor/xiaomi/sm8350-common 
+git clone https://github.com/Failedmush1/android_kernel_xiaomi_sm8350 -b Resuki  kernel/xiaomi/sm8350  
+git clone https://github.com/Failedmush1/hardware_xiaomi -b lineage-23.2 hardware/xiaomi 
+git clone https://github.com/failedmush1/hardware_dolby -b Dolby-Vision-2.6 hardware/dolby
