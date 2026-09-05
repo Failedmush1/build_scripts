7f6216a8d0ee7b150f1ffe5749d905433d030eeb
Failedmush <163705497+Failedmush@users.noreply.github.com>
2026-09-05 14:54:02 +0900

    Update LosA13.sh

--- LosA13.sh
+++ LosA13.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/LineageOS/android -b lineage-20.0 --git-lfs --depth=1
+repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-20.0 --git-lfs --depth=1
 rm -rf prebuilts/clang/host/linux-x86
 repo sync 
 git clone https://github.com/Failedmush1/android_device_xiaomi_renoir -b lineage-20 device/xiaomi/renoir
